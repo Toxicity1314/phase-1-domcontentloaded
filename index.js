e3b9ba82c920f44429e8545bf0782c32729a9304
Sean Welsh <s.welsh1314@gmail.com>
2022-11-23 09:40:11 -0500

    commit tree 5aaf276004b8cba63512814d83734cd0ba870c92

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () =>{
+    const p = document.querySelector('#text');
+    console.log(p)
+    p.textContent='This is really cool!'
+})
